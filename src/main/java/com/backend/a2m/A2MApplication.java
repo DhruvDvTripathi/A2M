23c58ea94cda8b6a20f64f5333f103c6ff2bdd9c
DhruvDvTripathi <dhruvdvtripathi@gmail.com>
2024-02-08 15:46:03 +0530

    "initial commit"

---
+++ src/main/java/com/backend/a2m/A2MApplication.java
@@ -0,0 +1,13 @@
+package com.backend.a2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2MApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2MApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/a2m/A2MApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.a2m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A2MApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
